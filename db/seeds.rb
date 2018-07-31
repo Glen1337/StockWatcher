6d66e45933fe1825756a4af6cd6dd694308f141b
Glen1337 <glen_anderson@student.uml.edu>
2018-07-30 21:27:54 -0400

    added serializer

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+user1 = User.create!(email: "mike@gmail.com", password: "password")
+user2 = User.create(email: "bob@gmail.com", password: "123456")
+user3 = User.create(email: "hey@hey.com", password: "welcome")
+user4 = User.create(email: "example@yahoo.com", password: "twitter")
+
+user1
+
 stock1 = StockHolding.create!(ticker: "INTC", cost_basis: 48, quantity: 1000)
 stock2 = StockHolding.create(ticker: "FEYE", cost_basis: 72.643, quantity: 32.6)
 stock3 = StockHolding.create(ticker: "PANW", cost_basis: 90.32, quantity: 70.0)
 stock3 = StockHolding.create(ticker: "M", cost_basis: 47, quantity: 5)
 
 
-port1 = Portfolio.create(name: "CompSec", stock_holdings: [])
+port1 = Portfolio.create!(name: "CompSec", stock_holdings: [])
 port2 = Portfolio.create(name: "Primary")
-
-user1 = User.create!(email: "mike@gmail.com", password: "123456")
-user2 = User.create(email: "bob@gmail.com", portfolios: "123456")
