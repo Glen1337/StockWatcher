2462b409b22a34d06ee96c5cf55827eb6590f268
Glen1337 <glen_anderson@student.uml.edu>
2018-08-01 22:39:05 -0400

    increase decimal scale

--- db/migrate/20180726004004_create_stock_holdings.rb
+++ db/migrate/20180726004004_create_stock_holdings.rb
@@ -3,8 +3,8 @@ class CreateStockHoldings < ActiveRecord::Migration[5.2]
     create_table :stock_holdings do |t|
       t.belongs_to :portfolio
       t.string :ticker, null: false, unique: true
-      t.decimal :quantity, null: false, scale: 6, precision: 12
-      t.decimal :cost_basis, null: false, scale: 6, precision: 12
+      t.decimal :quantity, null: false, scale: 7, precision: 12
+      t.decimal :cost_basis, null: false, scale: 7, precision: 12
       t.timestamps null: false
     end
   end
