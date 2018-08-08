972d81ac4b72873c887507b5bfefd7f1e9233e67
Glen1337 <glen_anderson@student.uml.edu>
2018-08-08 13:08:23 -0400

    separate volume chart done

--- app/javascript/react/containers/StockContainer.js
+++ app/javascript/react/containers/StockContainer.js
@@ -59,13 +59,21 @@ class StockContainer extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState){
-    var table, mapping, chart;
+
 
     if (this.state.currentPrices.length > 1 && this.state.show){
+      let table, mapping, chart;
+      // Clear container div
       this.refs.myInput.innerHTML = '';
+
+      // Create whole chart
+      chart = anychart.stock();
+
+      // Make data table
       table = anychart.data.table();
       table.addData(this.state.currentPrices);
 
+      // Mapping for price data
       mapping = table.mapAs();
       mapping.addField('open', 1, 'first');
       mapping.addField('high', 2, 'max');
@@ -73,29 +81,39 @@ class StockContainer extends React.Component {
       mapping.addField('close', 4, 'last');
       mapping.addField('value', 4, 'last');
 
-      chart = anychart.stock();
+      // Mapping for volume data
+      let volMapping = table.mapAs({'value': 5});
 
-      // dashed horizontal grid
-      let plot_1 = chart.plot(0)
+      // Price plot
+      let plot_1 = chart.plot(0);
 
+      // Volume plot
+      let plot_2 = chart.plot(1);
+
+      // Change crosshair, grid, and x-axis labels for first plot
       plot_1.yGrid().enabled(true);
       plot_1.crosshair().xStroke("#A9A9A9", 1.5, "10 5", "round");
       chart.plot(0).yGrid().stroke({dash: "5 25"});
-
       let xAxis = chart.plot(0).xAxis();
       xAxis.labels().position('right').anchor('left_center');
       xAxis.minorLabels().position('right').anchor('left_center');
       xAxis.background('#ADD8E6');
-      xAxis.height(20);
+      xAxis.height(40);
 
+      // Set series type for volume chart
+      chart.plot(1).column(volMapping).name('Volume');
+
+      // Set the series type
       chart.plot(0).candlestick(mapping).name('Company');
       // let grouping = chart.grouping();
       // grouping.minPixPerPoint(1);
       debugger;
+      // Chart title
       // state and prevState not working here
       chart.title(this.state.stockTicker);
       chart.title(prevState.stockTicker);
       chart.container('container');
+
       chart.draw();
       this.setState({show: false})
     }
