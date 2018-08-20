823971169cdfadc9a0af177746de77d6bd667ca8
Glen1337 <glen_anderson@student.uml.edu>
2018-08-20 14:01:55 -0400

    user model testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,30 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
-  describe "user" do
-    let!(:user1) { FactoryBot.create(:user) }
-    it "user has a username" do
-      user = FactoryBot.create(:user)
 
+  describe "validations and initializations" do
+    # let!(:user1) { FactoryBot.build(:user) }
+    let (:user){
+      User.new(username: "testuser", email: "testuser@sample.com", password: "passwordtest")
+    }
+
+    it "user is valid" do
+      #user = FactoryBot.build(:user)
+      #expect(user).to be_valid
       expect(user).to be_valid
-      expect(user1).to be_valid
-
     end
+
+    it "user has a username" do
+      expect(user.username).to eq("testuser")
+      expect(user).to be_valid
+
+      user.username = ""
+      expect(user).to_not be_valid
+    end
+
+    it "user has default initial balance of 10000" do
+      expect(user.balance).to eq (10000)
+    end
+
   end
 end
